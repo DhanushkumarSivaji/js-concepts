9d9847e7e4c20da07b5de03bf378d249e3623b69
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2022-02-02 12:14:29 +0530

    studied about closures

--- closures/script.js
+++ closures/script.js
@@ -1,4 +1,4 @@
-// function along with its lexical environment bundled together forms the closure
+// function along with its lexical environment bundled together forms the closure.In other words, a closure gives you access to an outer function’s scope from an inner function
 
 function x() {
     var a = 7;
